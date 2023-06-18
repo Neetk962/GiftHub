d53f9265b9d2929cd7e437a250c41eabceb15e55
jtec <jeff.tecson@yahoo.com>
2023-06-17 17:32:39 -0700

    Made see products, added the links & their names.

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,34 +7,129 @@ const addressEl = document.getElementById("address")
 var storesButton = document.getElementById("storesButton")
 
 const stores = [
-    { 
+    {
+        id: "arcteryx",
         store: "Arc’teryx",
-        address: "Address: 356 Santana Row, Suite 1001 | Phone: 408-642-1611"
+        address: "Address: 356 Santana Row, Suite 1001 | Phone: 408-642-1611",
+        products: [
+            { 
+                name: 'norvan vest',
+                url: 'https://www.arcteryx.com/us/en/shop/norvan-14-vest'
+            },
+            { 
+                name: 'granville',
+                url: 'https://www.arcteryx.com/us/en/shop/granville-crossbody-bag'
+            },
+            { 
+                name: 'nuclei',
+                url: 'https://www.arcteryx.com/us/en/shop/mens/nuclei-vest'
+            }            
+        ]
     },
     {
+        id: "athleta",
         store: "Athleta",
-        address: "Address: 378 Santana Row | Phone: 408-538-1208"
+        address: "Address: 378 Santana Row | Phone: 408-538-1208",
+        products: [
+            { 
+                name: 'brooklyn',
+                url: 'https://athleta.gap.com/browse/product.do?pid=534779032&cid=89745&pcid=89745&vid=1&nav=meganav%3ADresses%3ACATEGORIES%3AAll%20Dresses%20%26%20Rompers#pdp-page-content'
+            },
+            { 
+                name: 'excursion',
+                url: 'https://athleta.gap.com/browse/product.do?pid=882790022&cid=1032096&pcid=1032096&vid=1&nav=meganav%3AAccessories%3ACATEGORIES%3AAll%20Accessories#pdp-page-content'
+            },
+            { 
+                name: 'poplin',
+                url: 'https://athleta.gap.com/browse/product.do?pid=587913032&cid=1032080&pcid=1032080&vid=1&nav=meganav%3ATops%3ACATEGORIES%3AAll%20Tops#pdp-page-content'
+            }            
+        ]
     },
     {
+        id: "harajuku",
         store: "Boutique Harajuku",
-        address: "Address: 378 Santana Row Suite 1025 | Phone: 408-260-2150"
+        address: "Address: 378 Santana Row Suite 1025 | Phone: 408-260-2150",
+        products: [
+            { 
+                name: 'frill',
+                url: 'https://www.harajukuboutique.com/product-page/pattern-frill-dress'
+            },
+            { 
+                name: 'magic',
+                url: 'https://www.harajukuboutique.com/product-page/magic-jacket'
+            },
+            { 
+                name: 'candice',
+                url: 'https://www.harajukuboutique.com/product-page/candice-o-less-1'
+            }            
+        ]
     },
     {
+        id: "kendrascott",
         store: "Kendra Scott",
-        address: "Address: 334 Santana Row, Suite 1070 | Phone: 669-231-0300"
+        address: "Address: 334 Santana Row, Suite 1070 | Phone: 669-231-0300",
+        products: [
+            { 
+                name: 'susie',
+                url: 'https://www.kendrascott.com/new/shop-all-new-arrivals/susie-pendant-necklace-layering-set-of-2/susie-layering-set.html?dwvar_susie-layering-set_metal=GLD'
+            },
+            { 
+                name: 'emilie',
+                url: 'https://www.kendrascott.com/gifts-150-and-under/emilie-bracelet-set-of-2/emilie-bracelet-set.html?dwvar_emilie-bracelet-set_stoneColor=970'
+            },
+            { 
+                name: 'madison',
+                url: 'https://www.kendrascott.com/madison-daisy-earring-set/madison-earring-set.html?dwvar_madison-earring-set_metal=GLD'
+            }            
+        ]
     }
-    ]
+]
 
-storesButton.addEventListener("click", function(event){
+storesButton.addEventListener("click", function (event) {
     showStores(stores)
 })
 
 function showStores(stores) {
     allStores = ``
-    for (var i = 0; i < stores.length; i++){
-    allStores +=`<p>${stores[i].store}</p>
-    `;
+    for (var i = 0; i < stores.length; i++) {
+        allStores += `<p>${stores[i].store}</p>`;
+        allStores += `<button id=${stores[i].id}Button>See products</button>`
+        allStores += `<div id=${stores[i].id}Products></div>`
     }
     allStoresEl.innerHTML = allStores
     console.log(stores)
+
+    stores.forEach(function(store) {
+        var seeProductsButton = document.getElementById(`${store.id}Button`);
+        seeProductsButton.addEventListener("click", function (event) {
+            console.log('opening' + store.id)
+            showProducts(store.id, store.products)
+        })
+
+    })
 }
+
+function showProducts(storeId, products) {
+    console.log("adding to " + storeId)
+    var productsEl = document.getElementById(`${storeId}Products`)
+    var allProducts = ``
+    for (var i = 0; i < products.length; i++) {
+        // allProducts += `<p>${products[i].name}</p>`
+        // allProducts += `<p> <a href="https://www.arcteryx.com/us/en/shop/norvan-14-vest">  Norvan Vest        </a> </p>`
+        allProducts += `<p> <a href="${products[i].url}">   ${products[i].name}  </a> </p>`
+    }
+    productsEl.innerHTML = allProducts
+}
+
+// "Jeff,Alan,Alvin"
+
+// guestlist = ""
+// guestlist += "Jeff" // "Jeff"
+// guestlist = guestlist + ",Alan"
+
+// guestlist += "whattoadd"
+// guestlist = guestlist + "whattoadd"
+
+// guestlist += ",Alan" // "Jeff,Alan"
+// guestlist += ",Alvin" // "Jeff,Alan,Alvin"
+// guestlist += ",Alan" // "Jeff,Alan"
